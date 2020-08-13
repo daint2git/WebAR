881088d6304525b30f05ed2fe586a31505efdcc8
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2020-08-13 11:31:01 +0700

    update

--- demo/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-app.use(express.static('./'))
-
-app.listen(80, () => {
-  console.log(`Example app listening at http://localhost:${80}`)
-})
